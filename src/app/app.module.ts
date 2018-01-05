e1b5827832c486f95116b0bf0c99111cba89170e
James Knippel <jamesknippel@gmail.com>
2018-01-04 17:26:39 -0800

    refactored to router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './dashboard/header/header.component';
 import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
 import { DocumentViewComponent } from './dashboard/document-view/document-view.component';
+import { LandingComponent } from './dashboard/landing-component/landing-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    DocumentViewComponent
+    DocumentViewComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
